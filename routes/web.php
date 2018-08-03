dd84295453c5c67335d1a5a2b46b6b729439a6a4
2416108228 <2416108228@qq.com>
2018-08-03 09:11:56 +0800

    修改web.php冲突

--- routes/web.php
+++ routes/web.php
@@ -20,53 +20,37 @@ Route::get('captcha/show', 'CaptchaController@show');
 // 后台分组路由
 Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
 
-	// 后台登陆页面
-	Route::get('user/login', 'UserController@login');
+    // 后台登陆页面
+    Route::get('user/login', 'UserController@login');
 
-	// 后台登陆检查
-	Route::post('user/check', 'UserController@check');
+    // 后台登陆检查
+    Route::post('user/check', 'UserController@check');
 
-	// 后台退出操作
-	Route::get('user/logout', 'UserController@logout');
+    // 后台退出操作
+    Route::get('user/logout', 'UserController@logout');
 
-	// 后台首页
-	Route::get('/', 'AdminController@index');
+    // 后台首页
+    Route::get('/', 'AdminController@index');
 
-	// 后台用户列表
-	Route::get('user/list', 'UserController@list');
+    // 后台用户列表
+    Route::get('user/list', 'UserController@list');
 
-	// ajax后台展示用户添加页面
-	Route::get('user/add', 'UserController@add');
+    // ajax后台展示用户添加页面
+    Route::get('user/add', 'UserController@add');
 
-	// 后台用户根据id编辑
-	Route::get('user/edit/{id}', 'UserController@edit');
+    // 后台用户根据id编辑
+    Route::get('user/edit/{id}', 'UserController@edit');
 
-	// 后台用户根据id更新
-	Route::post('user/edit/{id}', 'UserController@edit');
+    // 后台用户根据id更新
+    Route::post('user/edit/{id}', 'UserController@edit');
 
-	// 后台用户根据id删除
-	Route::get('user/del/{id}', 'UserController@del');
-
-	//=================================================================
-
-<<<<<<< HEAD
-	Route::get('inventory_category/add', 'Inventory_categoryController@add');
-=======
-    Route::get('inventory_category/add', 'Inventory_categoryController@add');
->>>>>>> a265aa9130f65f5fe328e8c656ea0e06ee605e03
+    // 后台用户根据id删除
+    Route::get('user/del/{id}', 'UserController@del');
 
     //=================================================================
+    Route::get('inventory_category/add', 'Inventory_categoryController@add');
 
-<<<<<<< HEAD
-	// 后台管理商品模块--商品分类列表
-	Route::get("productCategory/list", "ProductCategoryController@list");
-	Route::get("productCategory/add", "ProductCategoryController@add");
-
-	//=================================================================
-
-	// 后台管理用户模块--普通用户分类列表
-	Route::get('user/index', "UserController@index");
-=======
+    //=================================================================
     // 后台管理商品模块--商品分类列表
     Route::get("productCategory/list", "ProductCategoryController@list");
 
@@ -75,8 +59,7 @@ Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
 
     // 后天管理商品模块--添加商品分类
     Route::post("productCategory/doAdd", "ProductCategoryController@doAdd");
->>>>>>> a265aa9130f65f5fe328e8c656ea0e06ee605e03
 
-	// 后台管理用户模块--员工分类列表
-	Route::get('user/staff', "UserController@staff");
+    // 后台管理用户模块--员工分类列表
+    Route::get('user/staff', "UserController@staff");
 });
